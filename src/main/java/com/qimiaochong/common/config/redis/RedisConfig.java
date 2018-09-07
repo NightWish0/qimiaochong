6919121be02ea86353491344cf8216ecc7d16269
XuXin <xuxin@dingyantech.com>
2018-09-07 17:07:15 +0800

    修正

--- src/main/java/com/qimiaochong/common/config/redis/RedisConfig.java
+++ src/main/java/com/qimiaochong/common/config/redis/RedisConfig.java
@@ -3,8 +3,6 @@ package com.qimiaochong.common.config.redis;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
